f53c09f424c55ff78e1fdbfe0bcdaa8308b4170b
Benjamin Kleynhans <bxk8027@rit.edu>
2018-01-22 23:02:05 -0500

    Created Student class - WIP

---
+++ ListOfObjects/ListOfObjects/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfObjects
+{
+    class Student
+    {
+        private string fullName;
+        private string major;
+        private int yearOfBirth;
+
+        public string FullName
+        {
+            set { fullName = value; }
+            get { return this.fullName; }
+        }
+
+        public string Major
+        {
+            set { major = value; }
+            get { return this.major; }
+        }
+
+        public int YearOfBirth
+        {
+            set { yearOfBirth = value; }
+            get { return this.yearOfBirth; }
+        }
+    }
+}
